b5ad6532326d9d4b23688c1ada4b616dfdcec078
byrwinter <41683082+byrwinter@users.noreply.github.com>
2018-07-25 15:15:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import telegram
-from telegram.ext import Updater, Dispatcher
-from telegram.ext import Handler, CommandHandler, MessageHandler, BaseFilter, Filters
-from telegram import MessageEntity
-from myHandlers import *
+from telegram.ext import Updater, Dispatcher, CommandHandler, Handler
+import os
+
 
 
 bot = telegram.Bot(token="666720872:AAF-px3ihfHBXSscVnehO_hB7NIVNn7q6QY")
@@ -10,12 +9,24 @@ updater = Updater(token="666720872:AAF-px3ihfHBXSscVnehO_hB7NIVNn7q6QY")
 dispatcher = updater.dispatcher
 
 
+TOKEN = "666720872:AAF-px3ihfHBXSscVnehO_hB7NIVNn7q6QY"
+PORT = int(os.environ.get('PORT', '8443'))
 
-startHandler = CommandHandler('start', startHandle)
+
+def echo(bot, update):
+  bot.send_message(chat_id=update.message.chat_id, text="update.message.text")
+
+
+
+startHandler = CommandHandler('start', echo)
 dispatcher.add_handler(startHandler)
 
-subMsgHandler = MessageHandler(Filters.text, subMsgHandle)
-dispatcher.add_handler(subMsgHandler)
 
 
-updater.start_polling()
+# add handlers
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://initot.com/" + TOKEN)
+updater.idle()
+
