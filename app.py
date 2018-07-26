7d70cb984da4ea9e83ae692d86c1932a417604c8
byrwinter <41683082+byrwinter@users.noreply.github.com>
2018-07-26 13:03:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,26 +17,58 @@ TOKEN = "666720872:AAF-px3ihfHBXSscVnehO_hB7NIVNn7q6QY"
 PORT = int(os.environ.get('PORT', '8443'))
 
 
-def echo(bot, update):
-  bot.send_message(chat_id=update.message.chat_id, text="update.message.text")
-
-
-
-startHandler = CommandHandler('start', echo)
-dispatcher.add_handler(startHandler)
+import telegram
+from telegram.ext import Updater, Dispatcher
+from telegram.ext import Handler, CommandHandler, MessageHandler, BaseFilter, Filters
+from telegram import MessageEntity
+from myHandlers import *
+import os
 
 
 bot = telegram.Bot(token="666720872:AAF-px3ihfHBXSscVnehO_hB7NIVNn7q6QY")
 updater = Updater(token="666720872:AAF-px3ihfHBXSscVnehO_hB7NIVNn7q6QY")
 dispatcher = updater.dispatcher
 
-
-
 startHandler = CommandHandler('start', startHandle)
 dispatcher.add_handler(startHandler)
 
+
+#add admin filter
+class add_filter(BaseFilter):
+    def filter(self, message):
+        return 'Add Admin' in message.text
+
+addFilter = add_filter()
+
+#add admin handler
+addHandler = MessageHandler(addFilter, adminHandle)
+dispatcher.add_handler(addHandler)
+
+
+
+#reply messages filter
+class rep_filter(BaseFilter):
+    def filter(self, message):
+        return 'Reply Messages' in message.text
+
+repFilter = rep_filter()
+
+#reply messages handler
+repHandler = MessageHandler(repFilter, adminHandle)
+dispatcher.add_handler(repHandler)
+
+#reply foward hander
+replyMsgHandler = MessageHandler(Filters.reply, replyMsgHandle)
+dispatcher.add_handler(replyMsgHandler)
+
+#subMessages handler
 subMsgHandler = MessageHandler(Filters.text, subMsgHandle)
 dispatcher.add_handler(subMsgHandler)
+
+
+
+
+
 
 # add handlers
 updater.start_webhook(listen="0.0.0.0",
